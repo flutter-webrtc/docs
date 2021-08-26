6a0c0b9de4fb1151ad70367dd9110ca54ba85436
cloudwebrtc <duanweiwei1982@gmail.com>
2021-08-26 13:26:09 +0800

    first commit.

---
+++ docs/docs.md
@@ -0,0 +1,11 @@
+---
+sidebar_position: 1
+---
+
+# Flutter Webrtc Docs
+
+Let's discover **Docusaurus in less than 5 minutes**.
+
+## Getting Started
+
+Get started by **creating a new site**.
---
+++ docusaurus.config.js
@@ -0,0 +1,115 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Flutter WebRTC Community',
+  tagline: 'Make WebRTC/VoIP development easier.',
+  url: 'https://flutterwebrtc.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'flutter-webrtc', // Usually your GitHub org/user name.
+  projectName: 'flutter-webrtc', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Flutter WebRTC Community',
+      logo: {
+        alt: 'Flutter WebRTC',
+        src: 'img/flutter-webrtc.png',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'docs',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/flutter-webrtc',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Join Slack Channel',
+              href: 'https://join.slack.com/t/flutterwebrtc/shared_invite/zt-q83o7y1s-FExGLWEvtkPKM8ku_F8cEQ',
+            },
+            {
+              label: 'Chat on Gitter',
+              href: 'https://gitter.im/flutter-webrtc/Lobby?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/cloudwebrtc',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/flutter-webrtc',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Flutter-WebRTC Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
