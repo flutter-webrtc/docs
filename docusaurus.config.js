9a599c6061c322cb7a9a61819aa4c133644738ca
cloudwebrtc <duanweiwei1982@gmail.com>
2021-08-26 21:31:22 +0800

    Correct the editing path of docs and blogs.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -100,13 +100,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/flutter-webrtc/docs/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/flutter-webrtc/docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
